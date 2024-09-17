1e282593391b25ac65213e4f9e427f39dd0fcff4
Pavan kumar <yanamandrapavan367@gmail.com>
2024-09-17 19:37:57 +0530

    Update wsgi.py

--- firstproject/wsgi.py
+++ firstproject/wsgi.py
@@ -1,3 +1,4 @@
+
 """
 WSGI config for firstproject project.
 
@@ -14,3 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstproject.settings')
 
 application = get_wsgi_application()
+
+app = application
